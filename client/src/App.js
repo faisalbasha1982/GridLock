237920524fce8a4ce92d033e7622590e00a60478
Development.dubai.5 <development.dubai.5@jobfixers.be>
2019-10-01 17:59:57 +0400

    added changes to register and navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React,{ Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from '../src/components/layout/Landing';
 import Navbar from '../src/components/layout/Navbar';
+import Login from '../src/components/auth/Login';
+import Register from '../src/components/auth/Register';
 
 import './App.css';
 
 const App = () => {
   return (
+    <Router>
     <Fragment>
       <Navbar />
-      <Landing />
+      <Route exact path="/" component={Landing} />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
     </Fragment>
+    </Router>
   );
 }
 
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,18 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
         <nav className="navbar bg-dark">
         <h1>
-          <a href="index.html"><i className="fas fa-code"></i> GridLock System</a>
+            <Link to="/">
+                <i className="fas fa-code"></i> GridLock System
+            </Link>
         </h1>
         <ul>
-          <li><a href="profiles.html">Users</a></li>
-          <li><a href="register.html">Register</a></li>
-          <li><a href="login.html">Login</a></li>
+          <li><a href="!#">Users</a></li>
+          <li><Link to="/">Register</Link></li>
+          <li><Link to="/">>Login</Link></li>
         </ul>
-      </nav>
-  
+      </nav>  
     );
 }
 
